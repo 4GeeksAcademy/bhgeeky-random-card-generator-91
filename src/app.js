7e1cdd40589fd750904b0416523a8cbfb61127cd
Brian Hernández González <brianhernandezgonz96@gmail.com>
2024-11-24 22:56:08 +0000

    [spain-91] feat:Random-card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+  let numCard = Math.floor(Math.random() * num.length);
+  document.getElementById("num").innerHTML = num[numCard];
+  
+  const icon = ["&#9830;", "&#9829;", "&#9824;", "&#9827;"];
+  const icon2 = ["&#9830;", "&#9829;", "&#9824;", "&#9827;"];
+  let iconCard = Math.floor(Math.random() * icon.length, icon2.length);
+  document.getElementById("icon").innerHTML = icon[iconCard];
+  document.getElementById("icon2").innerHTML = icon2[iconCard]
+  
 };
+//Temporizador//
+setTimeout(() => {
+  const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"];
+  let numCard = Math.floor(Math.random() * num.length);
+  document.getElementById("num").innerHTML = num[numCard];
+  
+  const icon = ["&#9830;", "&#9829;", "&#9824;", "&#9827;"];
+  const icon2 = ["&#9830;", "&#9829;", "&#9824;", "&#9827;"];
+  let iconCard = Math.floor(Math.random() * icon.length, icon2.length);
+  document.getElementById("icon").innerHTML = icon[iconCard];
+  document.getElementById("icon2").innerHTML = icon2[iconCard]
+  
+}, 10000);
+
+//botón cambiar carta//
+const changeCard = document.querySelector(".changeCard")
+
+  
+
+
+  
+
+  
+
+  
+  
+  
+  
+
+  
+  
+  
+
+
+
